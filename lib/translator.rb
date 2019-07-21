1c8720bda68bd707c3751d7bf17ee262c978f28b
Kimberly Bone <k_bone@uncg.edu>
2019-07-21 23:16:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,8 @@ def get_japanese_emoticon (file, e_emoticon)
   emojifile = load_library(file)
     if emojifile["get_emoticon"].include? (e_emoticon)
       return emojifile["get_emoticon"][e_emoticon]
+    else 
+      return "Sorry the emoticon you entered is not a known emoticon."
     end 
   #puts "Type the English emoticon you would like to translate."
   #english_e = gets.chomp!
