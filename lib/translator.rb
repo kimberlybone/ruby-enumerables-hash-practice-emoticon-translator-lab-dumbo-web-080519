2a9b6434f955724095c4b9cbabd6d890816c44c8
Kimberly Bone <k_bone@uncg.edu>
2019-07-21 21:45:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,11 +3,10 @@ require "yaml"
 def load_library (file)
   emojifile = YAML.load_file('emoticons.yml')
   hash = {"get_meaning" => {}, "get_emoticon" => {}}
-=begin 
-emojifile.each do |meaning, emoji| 
+  emojifile.each do |meaning, emoji| 
       hash["get_meaning"][emoji[1]] = meaning
       hash["get_emoticon"][emoji[0]] = meaning
-=end
+    end
   return hash
 end
 
