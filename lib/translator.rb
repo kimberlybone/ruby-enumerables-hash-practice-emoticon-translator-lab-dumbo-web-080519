4f96e005d795b99361c6994f50cddfbd0633945a
Kimberly Bone <k_bone@uncg.edu>
2019-07-21 21:23:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library (file)
+  emoji_file = YAML.load('emoticons.yml')
+  hash = {}
+      hash["get_meaning"] = {}
+      hash["get_emoticon"] = {}
+      hash["get_meaning"] = {file[1] => key}
+      hash["get_emoticon"] = {file[0] => key}
+  return hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file, emoticon)
+  load_library('emoticons.yml')
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  load_library('emoticons.yml')
 end
